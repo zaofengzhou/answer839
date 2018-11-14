c20dd09dade5a11ac112aadad0533cca0e0009c2
zfzhou <zzf15751774351@163.com>
2018-11-14 18:03:34 +0800

    add more

--- 2017/fifth.cpp
+++ 2017/fifth.cpp
@@ -12,7 +12,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define MAXN 200;
+const int MAXN = 200;
 
 typedef struct pNode
 {
@@ -31,9 +31,29 @@ int cmp(const void* a, const void* b) {
         return m.x1 - n.x1;
 }
 
-/*int main()
+int main()
 {
-    printf("Hello World!\n");
+    FILE *fp, *fout;
+    int i = 0, N;
+    Point a[MAXN];
+
+    fp = fopen("data.in", "r");
+    while(fscanf(fp, "%f", &a[i].x1)) {
+        fscanf(fp, "%f%f%f", &a[i].y1, &a[i].x2, &a[i].y2);
+        a[i].area = (a[i].y2-a[i].y1)*(a[i].x2-a[i].x1);
+        a[i].distance = a[i].x1*a[i].x1+a[i].y1*a[i].y1;
+        i++;
+    }
+    fclose(fp);
+
+    N = i;
+    qsort(a, N, sizeof(Point), cmp);
+
+    fout = fopen("data.out", "w");
+    for(i = 0; i < N; i++) {
+        fprintf(fout, "%f %f %f %f\n", a[i].x1, a[i].y1, a[i].y1, a[i].y2);
+    }
+    fclose(fout);
     return 0;
-}*/
+}
 
