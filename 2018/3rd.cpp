82619ffadb678b31081ab811305660bb18c8b91a
zfzhou <zzf15751774351@163.com>
2018-11-30 18:25:39 +0800

    add 2018 code

--- 2018/3rd.cpp
+++ 2018/3rd.cpp
@@ -10,6 +10,18 @@ using namespace std;
  * qsort() ?
  */
 
+/* printd函数: 打印十进制数n */
+void printd(int n)
+{
+    if(n < 0) {
+        putchar('-');
+        n = -n;
+    }
+    if(n / 10)
+        printd(n / 10);
+    putchar(n % 10 + '0');
+}
+
 void printArray(int a[], int n)
 {
     for (int i = 0; i < n; ++i) {
@@ -18,11 +30,13 @@ void printArray(int a[], int n)
     cout << endl;
 }
 
+
 int cmp2(const void* a, const void* b)
 {
     return ( *(int*)a - *(int*)b );
 }
 
+/*
 void swap(int v[], int i, int j)
 {
     int temp;
@@ -31,7 +45,7 @@ void swap(int v[], int i, int j)
     v[j] = temp;
 }
 
-/* qsort函数：以递增顺序对v[left] ...v[right]进行排序 */
+// qsort函数：以递增顺序对v[left] ...v[right]进行排序
 void qsort(int v[], int left, int right)
 {
     int i, last;
@@ -48,29 +62,58 @@ void qsort(int v[], int left, int right)
     qsort(v, left, last-1);
     qsort(v, last+1, right);
 }
+*/
 
-
-
-/* printd函数: 打印十进制数n */
-void printd(int n)
+int numcmp(char *s1, char *s2)
 {
-    if(n < 0) {
-        putchar('-');
-        n = -n;
-    }
-    if(n / 10)
-        printd(n / 10);
-    putchar(n % 10 + '0');
+    double v1, v2;
+    v1 = atof(s1);
+    v2 = atof(s2);
+    if(v1 < v2)
+        return -1;
+    else if(v1 > v2)
+        return 1;
+    else
+        return 0;
 }
 
+void swap(void *v[], int i, int j)
+{
+    void* temp;
+    temp = v[i];
+    v[i] = v[j];
+    v[j] = temp;
+}
+
+void qsort1(void *v[], int left, int right, int (*comp)(const void *,const void *))
+{
+    int i, last;
+
+    if(left >= right)
+        return;
+    swap(v, left, (left+right)/2);
+    last = left;
+    for(i = left+1; i <= right; i++)
+        if((*comp)(v[i], v[left]) < 0)
+            swap(v, ++last, i);
+    swap(v, left, last);
+    qsort1(v, left, last-1, comp);
+    qsort1(v, last+1, right, comp);
+}
+
+/*
 int main()
 {
+    //char *a[] = {"3", "5", "4", "2", "1"};
     int a[] = {3, 5, 4, 2, 1};
     printArray(a, 5);
     //qsort(a, 5, sizeof(int), cmp2);
     //printd(123);
-    qsort(a, 0, 4);
+    qsort1((void **)a, 0, 4, cmp2);
 
     printArray(a, 5);
+
     return 0;
 }
+ */
+
---
+++ 2018/4th.cpp
@@ -0,0 +1,94 @@
+//
+// Created by zfzhou on 11/30/2018.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+ * 定义函数cubeSqrt，其计算一个数的立方根，算法自定，但尽可能使用
+ * 算法复杂度低的算法。
+ */
+
+//Binary search method
+double mySqrt(double x)
+{
+    double l=0, r = x, m, precision = 1e-6;
+
+    if(x < 1) {
+        l = x;  // root is in the range [l, r]
+        r = 1;
+    }
+    while(l <= r) {
+        m = l + (r-l)/2;
+        if(abs(m*m - x) < precision)
+            return m;
+        else if(m*m > x)
+            r = m;
+        else
+            l = m;
+    }
+}
+
+//Newton-Raphson method
+double mySqrt1(double x) {
+    double precision = 1e-6;
+    double r = x;
+    while(abs(r * r - x) > precision) {
+        r = (r + x / r) / 2;
+    }
+    return r;
+}
+
+double cubeSqrt(double x)       //Binary search method
+{
+    double l = 0, r = x, precision = 0.000001;
+    double m;
+    if(x < 1)
+        r = 1;
+    while(l <= r) {
+        m = l + (r-l)/2;
+
+        if(abs(m*m*m - x) < precision)
+            return m;
+        else if(m*m*m > x){
+            r = m - precision;    //r减少精度值
+        } else {
+            l = m + precision;
+        }
+    }
+}
+
+//Newton-Raphson method
+double cubeSqrt1(double x) {
+    double precision = 1e-6;
+    double r = x;
+//    while(abs(r*r*r - x) > precision) {
+//        r = (2*r + x / (r*r) ) / 3;
+//    }
+    //未在精度范围内就迭代
+    while(abs(r*r*r - x) > precision) {
+        r = r - (r*r*r-x)/(3*r*r);
+    }
+    return r;
+}
+
+int main()
+{
+    printf("%f\n", sqrt(2));
+    printf("%f\n", mySqrt(2));
+    printf("%f\n", mySqrt1(2));
+//    printf("%f\n", sqrt(0.01));
+//    printf("%f\n", mySqrt(0.01));
+//    printf("%f\n", sqrt(3));
+//    printf("%f\n", mySqrt(3));
+    printf("%f\n", cbrt(27));
+    printf("%f\n", cubeSqrt(27));
+    printf("%f\n", cbrt(9));
+    printf("%f\n", cubeSqrt(9));
+    printf("%f\n", cubeSqrt1(9));
+    printf("%f\n", cbrt(0.001));
+    //printf("%f\n", cubeSqrt(0.001));
+    printf("%f\n", cubeSqrt1(0.001));
+//    printf("%f\n", 1e-6);
+    return 0;
+}
