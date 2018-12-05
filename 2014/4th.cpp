af8e4c9777196aa31a2bbcdba93c57e710681698
zfzhou <zzf15751774351@163.com>
2018-12-05 18:19:24 +0800

    add 2014 code

--- 2014/4th.cpp
+++ 2014/4th.cpp
@@ -4,26 +4,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+ * 按要求写split函数，该函数将一个字符串以某个字符串作为分隔符进行分割，分隔的结果由若干个字符串组成的数组。
+ */
 
-void split(char *str, char key, char res[][1000])
+int split(char *str, char key, char res[1000][1000])
 {
-    int i;
-    while(str!="\0") {
-        i = 0;
-        int j = 0;
-        if(*(str) != key) {
-            res[i][j++] = *(str++);
-        } else {
-            i++;
-            j = 0;
+    int i, index = 0, k = 0;
+
+    for(i = 0; str[i] != '\0'; i++) {   //遍历字符串str如果str[i]不等于分隔符就赋给res
+        if(str[i] != key) {
+            res[index][k++] = str[i];
+        }
+        if(str[i] == key || i == strlen(str)-1) //str[i]等于分隔符的时候，要赋字符串结尾符；并令index++,k=0
+        {
+            res[index][k] = '\0';  //注意将字符串结尾赋值'\0'
+            index++;
+            k = 0;
+            if(key != ' ')  //跳过空格
+                i++;
         }
     }
+
+    return index;
 }
 
 
+/*
 int main()
 {
-    char *str = "hello. world";
-    printf("%s", str);
+    char *str = "The. Big, and. During, spread.";
+    printf("%s\n", str);
+
+    char res[1000][1000] = {};
+    int cnt = split(str, '.', res);
+    printf("%d\n", cnt);
+    for(int i = 0; i < cnt; i++) {
+        printf("%s\n", res[i]);
+    }
     return 0;
-}
+}*/
--- 2014/5th.cpp
+++ 2014/5th.cpp
@@ -1,5 +1,76 @@
 //
 // Created by zfzhou on 12/4/2018.
 //
-#include <bits/stdc++.h>
-using namespace std;
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*
+ * 按要求写程序 对文本中的字符串排序
+ */
+#define MAXLENGTH 10000
+
+struct lineNode {
+    int hashValue;
+    int len;
+    char *str;
+};
+
+int hash(char *s)
+{
+    int len = strlen(s);
+    int sum = 0;
+    for(int i = 0; i < len; i++) {
+        if(s[i] >= '0' && s[i] <= '9' || s[i] >= 'a' && s[i] <= 'z' ||
+            s[i] >= 'A' && s[i] <= 'Z') {
+            sum += s[i];
+        }
+    }
+    return sum % 10007;
+}
+
+int cmp14(const void *a, const void *b) {
+    lineNode *m = (lineNode*)a;
+    lineNode *n = (lineNode*)b;
+    if(m->hashValue != n->hashValue) {
+        return m->hashValue - n->hashValue;
+    } else if(m->len != n->len) {
+        return n->len - m->len;
+    } else {
+        return strcmp(n->str, m->str);
+    }
+}
+
+
+//int main(int argc, char *argv[])
+int main()
+{
+    FILE *fin, *fout;
+//    int n = atoi(argv[1]);
+    int n = 3;
+    lineNode line[n];
+
+    fin = fopen("../2014/data.in", "r");
+
+
+    char str[MAXLENGTH+1];
+    int i = 0;
+
+    while(fscanf(fin, "%s", str) != EOF) {
+        line[i].hashValue = hash(str);
+        line[i].len = strlen(str);
+        line[i].str = (char*)malloc(sizeof(char)*(line[i].len+1));
+        strcpy(line[i].str, str);
+        i++;
+    }
+
+    qsort(line, n, sizeof(line[0]), cmp14);
+
+    fout = fopen("../2014/data.out", "w");
+    for(int i = 0; i < n; i++)
+    {
+        fprintf(fout, "%s\n", line[i].str);
+    }
+
+    return 0;
+}
